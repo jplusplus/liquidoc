d211a2f3776cf557149cd51430f3bddf5c05f1d1
pirhoo <pierre.romera@gmail.com>
2015-06-06 14:19:00 +0200

    Added orem ipsum markdown

--- src/app/components/doc-viewer/doc-viewer.directive.js
+++ src/app/components/doc-viewer/doc-viewer.directive.js
@@ -10,6 +10,19 @@ angular.module('liquidoc')
       },
       link: function(scope, attrs, element) {
         scope.doc = scope.docViewer;
+        scope.doc.template = [
+          "# Yolo title",
+          "## Subtitle",
+          "",
+          "Lorem markdownum demptos colorem sumere dantem. Nostri Turnus, ad undis virus,",
+          "vetitae semicaper ab speciem sopita **honesta** in simul et quot. Splendescunt",
+          "omnes, cumque, paravi videres currum, pro adeo monte veteres rege.",
+          "",
+          "Vera ipse Proetus partes, ex deducite nomina se fluunt tu venabula neque et",
+          "talis auxiliaris pudori arces, exercent. Inmemor iter, in [urbes",
+          "donavi](http://www.wedrinkwater.com/): tenuit pater inermis ut inposito est",
+          "inquit Cephaloque fervebant."
+        ].join("\n");
       }
     }
   })
